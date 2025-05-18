90855c00d31c3b4eb2309d826a2a28a5d3048736
Josef Podany <podany.pepa@gmail.com>
2025-05-18 10:54:32 +0200

    new: initdb

---
+++ initdb.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB() error {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSL"),
+	)
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("initDB: Failed to connect to database: %w", err)
+	}
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		return fmt.Errorf("initDB: AutoMigrate failed: %w", err)
+	}
+
+	return nil
+}
