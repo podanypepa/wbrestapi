e88ea5226e7f682bbe980fde0dda51a856689b9b
Josef Podany <podany.pepa@gmail.com>
2025-05-22 09:46:42 +0200

    new: refactoring healthcheck

--- pkg/api/healthcheck.go
+++ pkg/api/healthcheck.go
@@ -5,19 +5,27 @@ import (
 	"gorm.io/gorm"
 )
 
+// HealthCheckResponse ...
+type HealthCheckResponse struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
 func healthCheck(gormDB *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		sqlDB, err := gormDB.DB()
+
 		if err != nil || sqlDB.Ping() != nil {
-			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
-				"status": "unhealthy",
-				"error":  "database not reachable",
-			})
+			return c.
+				Status(fiber.StatusServiceUnavailable).
+				JSON(HealthCheckResponse{
+					Status: "unhealthy",
+					Error:  "database not reachable",
+				})
 		}
 
-		return c.JSON(fiber.Map{
-			"status": "ok",
+		return c.JSON(HealthCheckResponse{
+			Status: "ok",
 		})
-
 	}
 }
